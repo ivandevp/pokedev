7e7625209538e45f8d8d09981c7d435311a4cb6e
Ivan Medina <medinadiazivan@gmail.com>
2019-01-12 10:49:36 -0600

    Redux setup

---
+++ src/reducers/pokemons.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const pokemons = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default pokemons;
