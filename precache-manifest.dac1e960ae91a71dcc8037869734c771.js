9596e807911c02f942dec50423e744d4231213e6
Ivan Medina <medinadiazivan@gmail.com>
2019-01-12 12:37:29 -0600

    Updates

---
+++ precache-manifest.dac1e960ae91a71dcc8037869734c771.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c67cd24131812b6cabf4",
+    "url": "/pokedev/static/js/runtime~main.c67cd241.js"
+  },
+  {
+    "revision": "49f6082632bb04d9184a",
+    "url": "/pokedev/static/js/main.49f60826.chunk.js"
+  },
+  {
+    "revision": "d9801535d89ed8a9e1dc",
+    "url": "/pokedev/static/js/1.d9801535.chunk.js"
+  },
+  {
+    "revision": "49f6082632bb04d9184a",
+    "url": "/pokedev/static/css/main.224508ec.chunk.css"
+  },
+  {
+    "revision": "f94bb21e7c344a57be387fb986035936",
+    "url": "/pokedev/index.html"
+  }
+];
